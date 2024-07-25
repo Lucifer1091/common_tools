a26b439144d9ca72f650e3f0aca3122ba55a09ab
AliAman <alirizucharolia@gmail.com>
2024-07-25 10:57:24 +0500

    Form Extensions Updated

--- lib/extensions/form.dart
+++ lib/extensions/form.dart
@@ -10,4 +10,22 @@ extension FormStateX on GlobalKey<FormState>? {
   bool isNotValid() {
     return !isValid();
   }
+
+  void reset() {
+    if (this == null) return;
+
+    return this!.currentState?.reset();
+  }
+
+  void save() {
+    if (this == null) return;
+
+    return this!.currentState?.save();
+  }
+
+  Set<FormFieldState<Object?>>? validateGranularly() {
+    if (this == null) return null;
+
+    return this!.currentState?.validateGranularly();
+  }
 }
